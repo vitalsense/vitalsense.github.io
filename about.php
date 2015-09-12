397048bb618da64ce1f9bc70b3a2d29f626d5f57
quo-agis <dcc13@msstate.edu>
2015-09-12 01:46:22 -0500

    First upload

---
+++ about.php
@@ -0,0 +1,210 @@
+<!DOCTYPE HTML>
+<!--
+	Halcyonic by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>About the VitalSense Team</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+        <link rel="icon" type="image/png" href="images/favicon.png">
+        <style>
+			.ccTeamImage {
+				text-align: center;	
+			}
+			
+			#content .ccTeamImage h4, 
+			#content .ccTeamImage h5,
+			#content .ccTeamImage h5 a.ccResumeLink,
+			#content .ccTeamImage p a.ccResumeLink {
+				color: #435560;
+				line-height: 0em;
+			}
+			
+			#content .ccTeamImage h4 {
+				font-size: 1.4em;
+			}
+			
+			#content .ccTeamImage h5 {
+				font-size: 1.1em;
+			}
+			
+			#content .ccTeamImage h5 a.ccResumeLink {
+				font-size: 1em;	
+			}
+			
+			#content .ccTeamImage p {
+				font-family: Oxygen, Segoe, "Segoe UI", "DejaVu Sans", "Trebuchet MS", Verdana, sans-serif;	
+				font-size: 0.9em;
+			}
+						
+			.ccTeamImage img {
+				display: inline-block;	
+			}
+			
+			@media screen and (min-width: 737px) {
+				#content .row .ccCenterImage {
+					margin-left: 358px;
+				}
+			}
+			
+			@media screen and (min-width: 737px) and (max-width: 1200px) {
+				#content .row .ccCenterImage {
+					margin-left: 298px;	
+				}
+			}
+	</style>
+	</head>
+	<body class="subpage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header-wrapper">
+					<?php
+						echo file_get_contents("header.html");
+					?>
+				</div>
+
+			<!-- Content -->
+				<div id="content-wrapper">
+					<div id="content">
+						<div class="container">
+							<div class="row">
+                            	<div class="3u 12u(mobile)">
+                                	<section class="ccTeamImage">
+                                    	<img src="images/team/cody.jpg" class="bordered-feature-image">
+                                        <h4>Cody Smith, Team Leader</h4><br>
+                                        <h5>Electrical Engineer</h5><br>
+										<h5><a href="resumes/cody_resume.pdf" class="ccResumeLink" target="_blank">Resume</a></h5>
+									</section>
+								</div>
+                            	<div class="3u 12u(mobile)">
+                                	<section class="ccTeamImage">
+                                    	<img src="images/team/eric.jpg" class="bordered-feature-image">
+                                        <h4>Eric Easterling</h4><br>
+                                        <h5>Electrical Engineer</h5><br>
+										<h5><a href="resumes/eric_resume.pdf" class="ccResumeLink" target="_blank">Resume</a></h5>
+									</section>
+								</div>
+                            	<div class="3u 12u(mobile)">
+                                	<section class="ccTeamImage">
+                                    	<img src="images/team/chris.jpg" class="bordered-feature-image">
+                                        <h4>Chris Kenney</h4><br>
+                                        <h5>Computer Engineer</h5><br>
+										<h5><a href="resumes/chris_resume.pdf" class="ccResumeLink" target="_blank">Resume</a></h5>
+									</section>
+								</div>
+                            	<div class="3u 12u(mobile)">
+                                	<section class="ccTeamImage">
+                                    	<img src="images/team/cory.jpg" class="bordered-feature-image">
+                                        <h4>Cory Coward</h4><br>
+                                        <h5>Electrical Engineer</h5><br>
+										<h5><a href="resumes/cory_resume.pdf" class="ccResumeLink" target="_blank">Resume</a></h5>
+									</section>
+								</div>
+							</div>
+                            <div class="row">
+                            	<div class="5u 12u(mobile) ccCenterImage">
+                                	<section class="ccTeamImage">
+                                    	<img src="images/team/sherif.jpg" class="bordered-feature-image">
+                                        <h4>Dr. Sherif Abdelwahed</h4><br>
+                                        <h5>Faculty Advisor</h5><br>
+                                        <p>Sherif Abdelwahed is an Associate Professor in the Electrical and Computer Engineering Department at Mississippi State University,
+                                           where he teaches and conducts research in the area of computer engineering, with specific interests in embedded systems, self-managing
+                                           systems, distributed systems, computer architecture, model-integrated computing, and formal verification.</p>
+                                        <p>Visit Dr. Abdelwahed's website <a href='http://www.ece.msstate.edu/~sherif/' target='_blank' class="ccResumeLink">here</a>.</p>
+                                    </section>
+                                </div>
+                            </div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Footer -->
+				<div id="footer-wrapper">
+					<footer id="footer" class="container">
+						<div class="row">
+							<div class="8u 12u(mobile)">
+
+								<!-- Links -->
+									<section>
+										<h2>Links to Important Stuff</h2>
+										<div>
+											<div class="row">
+												<div class="3u 12u(mobile)">
+													<ul class="link-list last-child">
+														<li><a href="#">Neque amet dapibus</a></li>
+														<li><a href="#">Sed mattis quis rutrum</a></li>
+														<li><a href="#">Accumsan suspendisse</a></li>
+														<li><a href="#">Eu varius vitae magna</a></li>
+													</ul>
+												</div>
+												<div class="3u 12u(mobile)">
+													<ul class="link-list last-child">
+														<li><a href="#">Neque amet dapibus</a></li>
+														<li><a href="#">Sed mattis quis rutrum</a></li>
+														<li><a href="#">Accumsan suspendisse</a></li>
+														<li><a href="#">Eu varius vitae magna</a></li>
+													</ul>
+												</div>
+												<div class="3u 12u(mobile)">
+													<ul class="link-list last-child">
+														<li><a href="#">Neque amet dapibus</a></li>
+														<li><a href="#">Sed mattis quis rutrum</a></li>
+														<li><a href="#">Accumsan suspendisse</a></li>
+														<li><a href="#">Eu varius vitae magna</a></li>
+													</ul>
+												</div>
+												<div class="3u 12u(mobile)">
+													<ul class="link-list last-child">
+														<li><a href="#">Neque amet dapibus</a></li>
+														<li><a href="#">Sed mattis quis rutrum</a></li>
+														<li><a href="#">Accumsan suspendisse</a></li>
+														<li><a href="#">Eu varius vitae magna</a></li>
+													</ul>
+												</div>
+											</div>
+										</div>
+									</section>
+
+							</div>
+							<div class="4u 12u(mobile)">
+
+								<!-- Blurb -->
+									<section>
+										<h2>An Informative Text Blurb</h2>
+										<p>
+											Duis neque nisi, dapibus sed mattis quis, rutrum accumsan sed. Suspendisse eu
+											varius nibh. Suspendisse vitae magna eget odio amet mollis. Duis neque nisi,
+											dapibus sed mattis quis, sed rutrum accumsan sed. Suspendisse eu varius nibh
+											lorem ipsum amet dolor sit amet lorem ipsum consequat gravida justo mollis.
+										</p>
+									</section>
+
+							</div>
+						</div>
+					</footer>
+				</div>
+
+			<!-- Copyright -->
+				<?php
+					echo file_get_contents("footer.html");
+				?>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
